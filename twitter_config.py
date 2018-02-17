f062a34f74914929c6a2842fded337b94e4d9cfe
S.Mojtaba Banaie <mojtaba.banaie@gmail.com>
2018-02-17 18:59:00 +0330

    Last Result

--- twitter_config.py
+++ twitter_config.py
@@ -1,7 +1,7 @@
 #These information comes from Twitter API
 #Create a Twitter Account and Get These Information from apps.twitter.com
 
-consumer_key = 'TWITTER_APP_CONSUMER_KEY'
-consumer_secret = 'TWITTER_APP_CONSUMER_SECRET'
-access_token = 'TWITTER_APP_ACCESS_TOKEN'
-access_secret = 'TWITTER_APP_ACCESS_SECRET'
+consumer_key = 'qUqABnLA0lPyoLNFxV9OF5VFC'
+consumer_secret = 'OZJmXthjf5APTlvmdHUyGBvW6IBj3BqQGax1ZAwLqZFxzbJsba'
+access_token = '15257539-duK7OfwVvTih6r9bXsJQsB8fmEpMN7fFDzbIMhuGu'
+access_secret = '3dv7Nxnb5aqm6yOSNM9f9UQ6CePb6TJDwtIECf2zpdBKP'
